6772159b2d01f92bd55ff2900b0e2cf51e2a8b3c
Hirusha1-cpu <hirushafernando121@gmail.com>
2026-09-10 19:24:31 +0530

    Uodated

--- resources/js/Context/AuthContext.jsx
+++ resources/js/Context/AuthContext.jsx
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await api.post('/api/auth/login', { email, password });
+      const response = await api.post('/auth/login', { email, password });
       const { access_token, user } = response.data;
       localStorage.setItem('token', access_token);
       setToken(access_token);
